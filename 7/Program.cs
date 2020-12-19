add684826ca35abfa2424bd997ed3e1e9f335d11
Jurgielele <73314712+Jurgielele@users.noreply.github.com>
2020-12-19 16:28:48 +0100

    7

---
+++ 7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0; int b = 1;
+            int.TryParse(Console.ReadLine(), out a);
+            while (a > 0)
+            {
+                b *= a;
+                a--;
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
