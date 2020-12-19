cd970c5a50e4996c1a6b54fbde202425558fc2e7
Jurgielele <73314712+Jurgielele@users.noreply.github.com>
2020-12-19 16:52:02 +0100

    9

---
+++ 9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, sum = 0;
+            int.TryParse(Console.ReadLine(), out a);
+            while (a != 0)
+            {
+                sum += a % 10;
+                a /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
