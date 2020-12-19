c2ae163cb469c59ec950f6efbab671903fcba600
Jurgielele <73314712+Jurgielele@users.noreply.github.com>
2020-12-19 17:07:33 +0100

    10

---
+++ 10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, sum=0;
+            do
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                sum += a;
+
+            } while (a > 0);
+            Console.WriteLine(sum);
+        }
+    }
+}
