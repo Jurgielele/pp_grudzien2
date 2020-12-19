6753d73701711b86010a866925d6f3eaf825d4cd
Jurgielele <73314712+Jurgielele@users.noreply.github.com>
2020-12-19 16:43:16 +0100

    8

---
+++ 8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 2; 
+            int.TryParse(Console.ReadLine(), out a);
+            
+            while (b <= a)
+            {
+                if (a % b == 0)
+                {
+                    if (a == b)
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                        break;
+                    }
+                }
+                b++;
+            }
+        }
+    }
+}
